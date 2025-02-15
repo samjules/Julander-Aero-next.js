d6c0586997cf401dfc341b5641ac2e3921760234
samjules <samjulander@gmail.com>
2025-02-14 21:42:47 -0900

    ,

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
 import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
+import outputs from "../amplify_outputs .json";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
@@ -14,32 +14,65 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
+  const [aircraft, setAircraft] = useState<Array<Schema["Aircraft"]["type"]>>([]);
 
-  function listTodos() {
-    client.models.Todo.observeQuery().subscribe({
+  // Fetch and observe Todos
+  useEffect(() => {
+    const subscription = client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-  }
 
-  useEffect(() => {
-    listTodos();
+    return () => subscription.unsubscribe();
   }, []);
 
   function createTodo() {
-    client.models.Todo.create({
-      content: window.prompt("Todo content"),
+    const content = window.prompt("Enter Todo content:");
+    if (content) {
+      client.models.Todo.create({ content });
+    }
+  }
+
+  // Fetch and observe Aircraft
+  useEffect(() => {
+    const subscription = client.models.Aircraft.observeQuery().subscribe({
+      next: (data) => setAircraft([...data.items]),
     });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  function addAircraft() {
+    const name = window.prompt("Enter Aircraft Name:");
+    if (name) {
+      client.models.Aircraft.create({ name }); // Use 'name' instead of 'content'
+    }
   }
 
   return (
     <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
+      {/* Aircraft Section */}
+      <div>
+        <h1>Aircraft List</h1>
+        <button onClick={addAircraft}>+ Add Aircraft</button>
+        <ul>
+          {aircraft.map((plane) => (
+            <li key={plane.id}>{plane.name}</li> 
+          ))}
+        </ul>
+      </div>
+
+      {/* Todos Section */}
+      <div>
+        <h1>My Todos</h1>
+        <button onClick={createTodo}>+ Add Todo</button>
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id}>{todo.content}</li>
+          ))}
+        </ul>
+      </div>
+
+      {/* Footer Section */}
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
