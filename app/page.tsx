1857ac3f87e6f161c9edb2b2fe13b9842aafd17d
samjules <samjulander@gmail.com>
2025-02-14 23:17:40 -0900

    1

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
+import "./../app/app.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs .json";
 import "@aws-amplify/ui-react/styles.css";
@@ -13,55 +14,48 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const [aircraft, setAircraft] = useState<Array<Schema["Aircraft"]["type"]>>([]);
 
-  useEffect(() => {
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
+  function listTodos() {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
-  }, []);
-
-  function createTodo() {
-    const content = window.prompt("Enter Todo:");
-    if (content) {
-      client.models.Todo.create({ content });
-    }
   }
 
   useEffect(() => {
-    client.models.Aircraft.observeQuery().subscribe({
-      next: (data) => setAircraft([...data.items]),
-    });
+    listTodos();
   }, []);
 
-  function addAircraft() {
-    const name = window.prompt("Enter Aircraft Name:");
-    const type = window.prompt("Enter Aircraft Type:");
-    if (name && type) {
-      client.models.Aircraft.create({ name, type });
-    }
+  function createTodo() {
+    client.models.Todo.create({
+      content: window.prompt("Todo content"),
+    });
   }
 
   return (
-    <main>
-      <div>
-        <h1>Aircraft List</h1>
-        <button onClick={addAircraft}>+ Add Aircraft</button>
-        <ul>
-          {aircraft.map((plane) => (
-            <li key={plane.id}>{plane.name} ({plane.type})</li>
-          ))}
-        </ul>
-      </div>
 
+
+
+    <main>
+      <h1>My todos</h1>
+      <button onClick={createTodo}>+ new</button>
+      <ul>
+        {todos.map((todo) => (
+          
+          <li onClick={() => deleteTodo(todo.id)} key={todo.id}>{todo.content}</li>
+        ))}
+      </ul>
+      
       <div>
-        <h1>My Todos</h1>
-        <button onClick={createTodo}>+ Add Todo</button>
-        <ul>
-          {todos.map((todo) => (
-            <li key={todo.id}>{todo.content}</li>
-          ))}
-        </ul>
+        🥳 App successfully hosted. Try creating a new todo.
+        <br />
+        <a href="https://docs.amplify.aws/nextjs/start/quickstart/nextjs-app-router-client-components/">
+          Review next steps of this tutorial.
+        </a>
       </div>
     </main>
   );
